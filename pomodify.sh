0d6e7ae592281e7ad242372d0e9f2baa095e301b
Bopit Towaranon <czarft@gmail.com>
2021-07-01 14:11:28 +0700

    change method auto play

--- pomodify.sh
+++ pomodify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_URI="https://open.spotify.com/user/125937873/playlist/5SgJR30RfzR5hO21TsQhBp"
+DEFAULT_URI="https://open.spotify.com/playlist/37i9dQZF1E3872lNNLjlE7?si=aae5a3fabb67435e"
 LOGFILE_PATH="./log_file.txt"
 POMO_WORK=25*60
 POMO_SH_BREAK=5*60
@@ -44,19 +44,21 @@ while :
 do 
 	if [ $COUNT -eq 4 ]; then
 		osascript -e 'display notification "Time for long break, cya in 15!" with title "Long Break"'
-		./spotify pause
+		spotify pause
 		call_logger
 		countdown $POMO_LN_BREAK
 		COUNT=0
 	else 
 		#Start Work
 		osascript -e 'display notification "Get to work..." with title "Work Time"'
-		./spotify play uri $DEFAULT_URI;
+		spotify play uri $DEFAULT_URI;
+		sleep 0.25  
+		spotify play
 		countdown $POMO_WORK;
 
 		#Start Break
 		osascript -e 'display notification "Time for short break, cya in 5!" with title "Short Break"'
-		./spotify pause
+		spotify pause
 		call_logger
 		countdown $POMO_SH_BREAK
 		((COUNT++));
